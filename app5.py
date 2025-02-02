9f4df32cf75e87063e98e15d704e941760f69cc8
Chaitanya Kakade <chaitanyakakade@Chaitanyas-MacBook-Pro.local>
2025-02-02 16:06:05 +0530

    modified greetings and thanks keywords list

--- app5.py
+++ app5.py
@@ -11,8 +11,49 @@ CORS(app)
 
 classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 authors = ["chaitanya","rishab"]
-greeting_keywords = ["hi", "hello", "hey", "how are you", "good morning", "good evening", "who are you", "what's up", "greetings", "hey there"]
-thank_you_goodbye_keywords = ["thanks", "thank you", "goodbye", "bye", "see you", "take care", "later"]
+greeting_keywords = [
+    "hi", "hello", "hey", "how are you", "good morning", "good evening",
+    "who are you", "what's up", "greetings", "hey there", "howdy",
+    "good afternoon", "good day", "sup", "yo", "what’s going on",
+    "long time no see", "nice to meet you", "pleased to meet you",
+    "how’s everything", "how’s it going", "how’s life", "how have you been",
+    "what’s new", "what’s happening", "how do you do", "hiya", "bonjour",
+    "hola", "namaste", "salutations", "aloha", "shalom", "ciao",
+    "good to see you", "peace", "wassup", "morning", "evening",
+    "hi there", "hello there", "how’s your day", "how’s your day going",
+    "how’s your morning", "how’s your evening", "any news", "yo yo",
+    "top of the morning", "how’s your week", "great to see you",
+    "welcome", "nice seeing you", "holla", "hallo", "hiya", "good vibes",
+    "hello friend", "hey friend", "hey buddy", "hello buddy", "hey mate",
+    "hello mate", "hey pal", "hey dude", "hey bro", "hey sis",
+    "hey fam", "hey there, how’s it going", "what’s poppin",
+    "what’s crackin", "how are things", "how’s the world treating you",
+    "hey sunshine", "good to hear from you", "happy to see you",
+    "hope you’re doing well", "what’s the latest", "hey stranger",
+    "cheers", "hey hey", "hello again"
+]
+
+thank_you_goodbye_keywords = [
+    "thanks", "thank you", "goodbye", "bye", "see you", "take care",
+    "later", "thanks a lot", "many thanks", "thanks so much",
+    "thanks a ton", "thanks a bunch", "thanks a million",
+    "thank you very much", "thank you kindly", "thank you so much",
+    "much appreciated", "deepest thanks", "endless thanks",
+    "I appreciate it", "I appreciate that", "appreciate it",
+    "appreciate that", "grateful", "big thanks", "huge thanks",
+    "with gratitude", "much obliged", "cheers", "ciao",
+    "farewell", "see ya", "see you later", "see you soon",
+    "catch you later", "talk to you later", "until next time",
+    "good night", "have a great day", "have a good one",
+    "take it easy", "stay safe", "peace", "peace out",
+    "so long", "adios", "au revoir", "sayonara", "hasta la vista",
+    "be well", "all the best", "best wishes", "godspeed",
+    "good luck", "safe travels", "see you around",
+    "until we meet again", "bye for now", "I gotta go",
+    "I’m out", "I’m off", "later gator", "smell ya later", "toodles", "keep in touch",
+    "don't be a stranger", "nice talking to you", "pleasure talking to you"
+]
+
 
 
 def is_greeting(question):
